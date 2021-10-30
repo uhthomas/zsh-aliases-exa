9865df52ec24a6324bae6f087c733ce57cb71ce2
Thomas <9749173+uhthomas@users.noreply.github.com>
2021-10-30 21:18:24 +0200

    match default ls behaviour and inherit aliases

--- zsh-aliases-exa.plugin.zsh
+++ zsh-aliases-exa.plugin.zsh
@@ -9,10 +9,10 @@
 #
 
 # general use
-alias ls='exa'                                                         # ls
+alias ls='exa --git --group-directories-first'                         # ls
 alias l='exa -lbF --git'                                               # list, size, type, git
 alias ll='exa -lbGF --git'                                             # long list
-alias llm='exa -lbGF --git --sort=modified'                            # long list, modified date sort
+alias llm='ll --sort=modified'                                         # long list, modified date sort
 alias la='exa -lbhHigUmuSa --time-style=long-iso --git --color-scale'  # all list
 alias lx='exa -lbhHigUmuSa@ --time-style=long-iso --git --color-scale' # all + extended list
 
